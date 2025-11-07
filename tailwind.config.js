ed87eb32e12621230a2de6b323459e8e007a846c
thesportifysociety <thesportifysociety@gmail.com>
2025-11-07 22:57:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: "media", // Changed from "class" to "media" to use system preference by default
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
